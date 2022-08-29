3b900628063af07f9c7f2f18c56ff69442c20e85
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-08-29 22:16:29 +0300

    GetLength completed

---
+++ GeometryTasks/GeometryTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X, Y;
+    }
+    public class Geometry
+    {
+        public static double GetLength(Vector victor)
+        {
+            return Math.Sqrt(victor.X * victor.X + victor.Y * victor.Y);
+        }
+
+        public static double Add(Vector victor, Vector vitek)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
