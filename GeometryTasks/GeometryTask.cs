688e3d85945440938bfef2a9c5651b3447e0abb9
Alyoshkin Dmitry <alyoshkin.dmitry@yandex.ru>
2022-08-30 20:41:34 +0300

    Segment completed

--- GeometryTasks/GeometryTask.cs
+++ GeometryTasks/GeometryTask.cs
@@ -14,11 +14,38 @@ namespace GeometryTasks
             return Math.Sqrt(victor.X * victor.X + victor.Y * victor.Y);
         }
 
+        public static double GetLength(Segment seg)
+        {
+            return GetLength(new Vector() {
+                X = seg.End.X - seg.Begin.X,
+                Y = seg.End.Y - seg.Begin.Y
+            });
+        }
+
+        public static bool IsVectorInSegment(Vector victor, Segment seg)
+        {
+            var segLength = GetLength(seg);
+            var beginLengthToVector = GetLength(new Segment(seg.Begin, victor));
+            var endLengthToVector = GetLength(new Segment(victor, seg.End));
+            return segLength == (beginLengthToVector + endLengthToVector);
+        }
+
         public static Vector Add(Vector victor, Vector vitek)
         {
             return new Vector() {
                 X = victor.X + vitek.X,
                 Y =  victor.Y + vitek.Y};
+        }
+    }
+
+    public class Segment
+    {
+        public Vector Begin, End;
+        public Segment() { }
+        public Segment(Vector begin, Vector end)
+        {
+            this.Begin = begin;
+            this.End = end;
         }
     }
 }
